bfc4c87fff763a8a84dae59717966ccea5b915f3
Noe <noe.r.guerra@outlook.com>
2020-10-14 00:07:32 -0400

    .

---
+++ theme_switch.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Tue Aug 18 07:01:40 2020
+
+@author: noerg
+"""
+
+import argparse
+import ctypes
+import os
+import winreg
+import wmi
+import yaml
+
+
+def load_settings():
+    try:
+        with open("settings.yaml") as file:
+            settings = yaml.load(file, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        with open("settings.yaml", "w") as file:
+            settings = {
+                "dark_mode": {
+                    "brightness": 0,
+                    "os_theme": 0,
+                    "wallpaper": "c:/Users/noerg/Pictures/dark_wallpaper.jpg",
+                    "start_hour": "07",
+                    "start_minute": "00",
+                    "enable_schedule": False
+                },
+                "light_mode": {
+                    "brightness": 100,
+                    "os_theme": 0,
+                    "wallpaper": "c:/Users/noerg/Pictures/light_wallpaper.jpg",
+                    "start_hour": "19",
+                    "start_minute": "00",
+                    "enable_schedule": False
+                },
+            }
+            yaml.dump(settings, file)
+    return settings
+
+
+def light_mode_is_on():
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+                         access=winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
+    return winreg.EnumValue(key, 2)[1]
+
+
+def change_brightness(value):
+    c = wmi.WMI(namespace='wmi')
+    methods = c.WmiMonitorBrightnessMethods()[0]
+    methods.WmiSetBrightness(value, 0)
+
+
+def change_wallpaper(path_to_wallpaper):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, path_to_wallpaper, 0)
+
+
+def change_apps_theme(value):
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+                         0, winreg.KEY_SET_VALUE)
+    winreg.SetValueEx(key, "AppsUseLightTheme", 0, winreg.REG_DWORD, value)
+    winreg.CloseKey(key)
+
+
+def change_system_theme(value):
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+                         0, winreg.KEY_SET_VALUE)
+    winreg.SetValueEx(key, "SystemUsesLightTheme", 0, winreg.REG_DWORD, value)
+    winreg.CloseKey(key)
+
+
+def create_task(start_dark_mode=None, start_light_mode=None):
+    if start_dark_mode:
+        os.popen(r'SCHTASKS /CREATE /SC DAILY /TN "Theme Switch\Change to Dark Mode" /TR ' +
+                 r'"python %userprofile%\Documents\test.py -d" /ST {0} /F'.format(start_dark_mode))
+    if start_light_mode:
+        os.popen(r'SCHTASKS /CREATE /SC DAILY /TN "Theme Switch\Change to Light Mode" /TR ' +
+                 r'"python %userprofile%\Documents\test.py -l" /ST {0} /F'.format(start_light_mode))
+
+
+def change_task_state(i, state):
+    if i == 0:
+        os.popen(r'SCHTASKS /CHANGE /TN "Theme Switch\Change to Dark Mode" /{0}'.format(state))
+    elif i == 1:
+        os.popen(r'SCHTASKS /CHANGE /TN "Theme Switch\Change to Light Mode" /{0}'.format(state))
+
+
+def task_exists(task_name):
+    if os.popen('schtasks /query /TN "Theme Switch\{0}" >NUL 2>&1 || echo error'.format(
+            task_name)).read().strip() == "error":
+        return False
+    return True
+
+
+def change_sys_theme(brightness, wallpaper, os_theme, **kwargs):
+    change_brightness(brightness)
+    change_wallpaper(wallpaper)
+    change_apps_theme(os_theme)
+    change_system_theme(os_theme)
+
+
+def check_tasks(settings):
+    tasknames = ("Change to Dark mode", "Change to Light mode")
+    modes = ("dark_mode", "light_mode")
+    for i, task_mode in enumerate(zip(tasknames, modes)):
+        if not task_exists(task_mode[0]):
+            start_time = f"{settings[task_mode[1]]['start_hour']}:{settings[task_mode[1]]['start_minute']}"
+            values = [None, None]
+            values[i] = f"{settings[task_mode[1]]['start_hour']}:{settings[task_mode[1]]['start_minute']}"
+            create_task(*values)
+        else:
+            query = os.popen(r'schtasks /query /TN "Theme Switch\{0}" /fo CSV /nh'.format(task_mode[0]))
+            output = query.readline().strip().replace('"', '')
+            output = output.split(',')
+            state = True if output[-1] == "Ready" else False
+            time = output[-2].strip().split(" ")
+            start_time = f"{settings[task_mode[1]]['start_hour']}:{settings[task_mode[1]]['start_minute']}"
+            if len(time) > 1 and time[1][:-3] != start_time:
+                start_time = time[:-3]
+                settings[task_mode[1]]['start_hour'] = start_time.split(":")[0]
+                settings[task_mode[1]]['start_minute'] = start_time.split(":")[1]
+            if state != settings[task_mode[1]]['enable_schedule']:
+                settings[task_mode[1]]['enable_schedule'] = state
+            with open('settings.yaml', 'w') as file:
+                yaml.dump(settings, file)
+
+
+def main():
+    settings = load_settings()
+    check_tasks(settings)
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--darkmode", action="store_const", const='dark_mode')
+    ap.add_argument("-l", "--lightmode", action="store_const", const='light_mode')
+    args = vars(ap.parse_args())
+    mode = args['darkmode'] or args['lightmode']
+
+    if type(mode) == str:
+        values = settings[mode]
+    else:
+        values = settings['dark_mode'] if light_mode_is_on() else settings['light_mode']
+    change_sys_theme(**values)
+
+    return
+
+
+if __name__ == "__main__":
+    pass
---
+++ theme_switch_gui.py
@@ -0,0 +1,322 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 07:24:36 2020
+
+@author: noerg
+"""
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkinter.filedialog import askopenfilename
+from PIL import Image, ImageTk
+from theme_switch import light_mode_is_on, change_sys_theme, load_settings, create_task, change_task_state
+import yaml
+import webbrowser
+import os
+
+"""
+TODO:
+    - Add install location and run commands from AppData/Roaming/ThemeSwitch [Not sure if needed]
+    - Clean code [Last step]
+"""
+
+
+class Base:
+    def __init__(self, parent):
+        self.parent = parent
+        self.parent.resizable(False, False)
+        self.load_theme()
+
+    def set_position(self, w, h):
+        """
+        Puts the window in the center of the screen.
+        
+        :param w: width of the window
+        :param h: height of the window
+        """
+        ws = self.parent.winfo_screenwidth()  # Screen width
+        hs = self.parent.winfo_screenheight()  # Screen height
+        x = (ws / 2) - (w / 2)  # Program x axis
+        y = (hs / 2) - (h / 2)  # Program y axis
+        self.parent.geometry('{0}x{1}+{2}+{3}'.format(w, h, int(x), int(y)))
+
+    def load_theme(self):
+        # Code based on https://stackoverflow.com/a/62934393
+        self.parent.tk.eval("""
+set base_theme_dir awthemes-9.3.2/
+
+package ifneeded awthemes 9.3.2 \
+    [list source [file join $base_theme_dir awthemes.tcl]]
+package ifneeded colorutils 4.8 \
+    [list source [file join $base_theme_dir colorutils.tcl]]
+package ifneeded awdark 7.7 \
+    [list source [file join $base_theme_dir awdark.tcl]]
+package ifneeded awlight 7.6 \
+    [list source [file join $base_theme_dir awlight.tcl]]
+                            """)
+        self.parent.tk.call("package", "require", "awdark")
+        self.parent.tk.call("package", "require", "awlight")
+
+
+class MainWindow(Base):
+    def __init__(self, parent):
+        Base.__init__(self, parent)
+        self.parent = parent
+        self.style = ttk.Style()
+
+        self.img = ImageTk.PhotoImage
+        self.canvas = tk.Canvas(self.parent)
+        self.action_btn = ttk.Button(self.parent)
+
+        self.initialize_interface()
+        Base.set_position(self, 200, 200)
+        self.get_active_mode()
+
+    def initialize_interface(self):
+        menubar = tk.Menu(self.parent)
+
+        settings_menu = tk.Menu(self.parent, menubar, tearoff=0)
+        settings_menu.add_command(label="Settings", command=self.open_settings_window)
+        menubar.add_cascade(label="Edit", menu=settings_menu)
+
+        about_menu = tk.Menu(self.parent, menubar, tearoff=0)
+        about_menu.add_command(label="About this program",
+                               command=self.open_about_window)
+        menubar.add_cascade(label="About",
+                            menu=about_menu)
+
+        self.canvas.config(width=200,
+                           height=150, )
+        self.canvas.pack()
+
+        self.action_btn.config(command=self.change_system_mode)
+        self.action_btn.pack(fill=tk.BOTH)
+
+        self.parent.config(menu=menubar)
+
+    def get_active_mode(self):
+        if light_mode_is_on():
+            self.apply_light_theme()
+        else:
+            self.apply_dark_theme()
+
+    def change_system_mode(self):
+        if light_mode_is_on():
+            mode = 'dark_mode'
+            self.apply_dark_theme()
+        else:
+            mode = 'light_mode'
+            self.apply_light_theme()
+        change_sys_theme(**load_settings()[mode])
+
+    def apply_dark_theme(self):
+        self.style.theme_use("awdark")
+        self.style.configure('TButton',
+                             relief=tk.FLAT)
+        self.img = ImageTk.PhotoImage(file="moon.png")
+        img_on_canvas = self.canvas.create_image(100, 75)
+        self.canvas.configure(background='black', highlightbackground='black')
+        self.canvas.itemconfig(img_on_canvas, image=self.img)
+        self.action_btn.config(text="Dark mode on")
+
+    def apply_light_theme(self):
+        self.style.theme_use("awlight")
+        self.style.configure('TButton',
+                             relief=tk.FLAT)
+        self.img = ImageTk.PhotoImage(file="sun.png")
+        img_on_canvas = self.canvas.create_image(100, 75)
+        self.canvas.configure(background='white', highlightbackground='white')
+        self.canvas.itemconfig(img_on_canvas, image=self.img)
+        self.action_btn.config(text="Light mode on")
+
+    def open_about_window(self):
+        new_window = tk.Toplevel(self.parent)
+        About(new_window)
+
+    def open_settings_window(self):
+        new_window = tk.Toplevel(self.parent)
+        Settings(new_window)
+
+
+class Settings(Base):
+    def __init__(self, parent):
+        Base.__init__(self, parent)
+        self.parent = parent
+
+        self.wallpaper_path = tk.StringVar(), tk.StringVar()
+        self.brightness_scale = tk.IntVar(), tk.IntVar()
+        self.start_hour = tk.StringVar(), tk.StringVar()
+        self.start_minute = tk.StringVar(), tk.StringVar()
+        self.enable_scheduler = tk.BooleanVar(), tk.BooleanVar()
+
+        tab_control = ttk.Notebook(self.parent)
+        self.tabs = {'dark_mode': ttk.Frame(tab_control),
+                     'light_mode': ttk.Frame(tab_control)}
+
+        self.preview_canvas = [tk.Canvas(self.tabs['dark_mode']), tk.Canvas(self.tabs['light_mode'])]
+        self.spin_state = ["disabled", "disabled"]
+        self.spin_hour = [tk.Spinbox(self.tabs['dark_mode']), tk.Spinbox(self.tabs['light_mode'])]
+        self.spin_minute = [tk.Spinbox(self.tabs['dark_mode']), tk.Spinbox(self.tabs['light_mode'])]
+        self.wallpaper_tk_thumbnail = [ImageTk.PhotoImage, ImageTk.PhotoImage]
+
+        self.initialize_interface(tab_control)
+        Base.set_position(self, 400, 250)
+
+    def initialize_interface(self, tab_control):
+        self.read_settings()
+
+        tab_control.add(self.tabs['dark_mode'], text='Dark mode')
+        tab_control.add(self.tabs['light_mode'], text='Light mode')
+        tab_control.pack(expand=1, fill=tk.BOTH)
+
+        self.create_settings_interface()
+
+    def create_settings_interface(self):
+        for i, tab in enumerate(self.tabs):
+            ttk.Label(self.tabs[tab],
+                      text="Wallpaper",
+                      anchor=tk.E).grid(row=0, column=0, sticky="e", padx=5, pady=2)
+            self.preview_canvas[i].config(width=96, height=48)
+            self.preview_canvas[i].bind("<Button-1>",
+                                        lambda event, i=i: self.get_wallpaper_path(i))
+            self.preview_canvas[i].grid(row=0,
+                                        column=1,
+                                        sticky="ew")
+            self.preview_img_on_canvas(self.wallpaper_path[i].get(), i)
+
+            ttk.Label(self.tabs[tab],
+                      text="Brightness",
+                      anchor=tk.E).grid(row=1, column=0, sticky="e", padx=5, pady=2)
+            ttk.Scale(self.tabs[tab],
+                      from_=0, to=100,
+                      orient=tk.HORIZONTAL,
+                      variable=self.brightness_scale[i],
+                      command=self.whole_values_only).grid(row=1, column=1, columnspan=2, sticky="ew")
+            ttk.Label(self.tabs[tab],
+                      text="0",
+                      textvariable=self.brightness_scale[i]).grid(row=1, column=3, sticky="w")
+
+            ttk.Label(self.tabs[tab],
+                      text="Run everyday at:",
+                      anchor=tk.E).grid(row=2, column=0, sticky="e", padx=5, pady=2)
+            self.spin_hour[i].config(textvariable=self.start_hour[i],
+                                     from_=0, to=23,
+                                     increment=1, wrap=True,
+                                     width=2,
+                                     format="%02.0f",
+                                     state=self.spin_state[i])
+            self.spin_hour[i].grid(row=2, column=1, sticky="w", padx=(0, 2))
+            self.spin_minute[i].config(textvariable=self.start_minute[i],
+                                       from_=0, to=59,
+                                       increment=1, wrap=True,
+                                       width=2,
+                                       format="%02.0f",
+                                       state=self.spin_state[i])
+            self.spin_minute[i].grid(row=2, column=2, sticky="w")
+            ttk.Checkbutton(self.tabs[tab],
+                            text="Enable task scheduling",
+                            variable=self.enable_scheduler[i],
+                            command=lambda i=i: self.update_spin(i)).grid(row=3, column=0, columnspan=5)
+            ttk.Button(self.tabs[tab],
+                       text="Cancel",
+                       command=self.parent.destroy).grid(row=5, column=0, columnspan=2)
+            ttk.Button(self.tabs[tab],
+                       text="Apply",
+                       command=self.apply_changes).grid(row=5, column=2, columnspan=2)
+
+    def get_wallpaper_path(self, i):
+        path = askopenfilename(parent=self.parent, initialdir=os.path.expanduser(r"~\Pictures"),
+                               filetypes=(("JPEG Files", "*.jpg *.jpeg"), ("PNG Files", "*.png")))
+        if path:
+            self.wallpaper_path[i].set(path)
+            self.preview_img_on_canvas(path, i)
+
+    def preview_img_on_canvas(self, img_path, i):
+        img = Image.open(img_path)
+        img.thumbnail((96, 48))
+        preview_img = self.preview_canvas[i].create_image(48, 24)
+        self.wallpaper_tk_thumbnail[i] = ImageTk.PhotoImage(img)
+
+        self.preview_canvas[i].itemconfig(preview_img,
+                                          image=self.wallpaper_tk_thumbnail[i])
+
+    def whole_values_only(self, e):
+        for i in [0, 1]:
+            value = self.brightness_scale[i].get()
+            self.brightness_scale[i].set(round(value))
+
+    def save_settings(self):
+        with open("settings.yaml", "r+") as file:
+            settings = yaml.load(file, Loader=yaml.FullLoader)
+        with open("settings.yaml", "w") as file:
+            for i, mode in enumerate(['dark_mode', 'light_mode']):
+                settings[mode]['brightness'] = self.brightness_scale[i].get()
+                if self.wallpaper_path[i].get():
+                    settings[mode]['wallpaper'] = self.wallpaper_path[i].get()
+                settings[mode]['start_hour'] = self.start_hour[i].get()
+                settings[mode]['start_minute'] = self.start_minute[i].get()
+                settings[mode]['enable_schedule'] = self.enable_scheduler[i].get()
+            yaml.dump(settings, file)
+
+    def read_settings(self):
+        with open("settings.yaml") as file:
+            settings = yaml.load(file, Loader=yaml.FullLoader)
+            for i, mode in enumerate(['dark_mode', 'light_mode']):
+                self.brightness_scale[i].set(settings[mode]['brightness'])
+                self.wallpaper_path[i].set(settings[mode]['wallpaper'])
+                self.start_hour[i].set(settings[mode]['start_hour'])
+                self.start_minute[i].set(settings[mode]['start_minute'])
+                self.enable_scheduler[i].set(settings[mode]['enable_schedule'])
+                if not settings[mode]['enable_schedule']:
+                    self.spin_state[i] = tk.DISABLED
+                else:
+                    self.spin_state[i] = 'readonly'
+
+    def update_spin(self, i):
+        marked = self.enable_scheduler[i].get()
+        self.spin_hour[i].config(state='readonly' if marked else tk.DISABLED)
+        self.spin_minute[i].config(state='readonly' if marked else tk.DISABLED)
+
+    def apply_changes(self):
+        self.save_settings()
+        for c, k in enumerate(["start_dark_mode", "start_light_mode"]):
+            settings = {"start_dark_mode": None, "start_light_mode": None}
+            settings[k] = f"{self.start_hour[c].get()}:{self.start_minute[c].get()}"
+            if self.enable_scheduler[c].get():
+                change_task_state(c, "ENABLE")
+                create_task(**settings)
+            else:
+                change_task_state(c, "DISABLE")
+
+        messagebox.showinfo("Changes applied",
+                            "Settings have been successfully updated . They will be applied next time you change modes",
+                            parent=self.parent)
+
+
+class About(Base):
+    def __init__(self, parent):
+        Base.__init__(self, parent)
+        self.parent = parent
+        self.frame = ttk.Frame(self.parent)
+        self.frame.pack()
+
+        ttk.Label(self.frame, text="Theme switch 1.0.0", font="Helvetica 10 bold").grid(row=0, column=0)
+        ttk.Label(self.frame, text="Programmed by Noé Guerra").grid(row=1, column=0)
+        ttk.Label(self.frame, text="Project page and source code available at:").grid(row=4, column=0)
+        link = ttk.Label(self.frame,
+                         text="https://github.com/Nxz02/ThemeSwitchWin10",
+                         foreground="SteelBlue3",
+                         cursor="hand2", )
+        link.grid(row=5, column=0)
+        link.bind("<Button-1>", lambda event: webbrowser.open_new(event.widget.cget("text")))
+
+
+def main():
+    root = tk.Tk()
+    root.iconbitmap("icon.ico")
+    MainWindow(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
