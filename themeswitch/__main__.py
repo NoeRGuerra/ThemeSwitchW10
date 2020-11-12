d08e960cf168fea7ccbb2927bb6e39297149d35c
Noe <noe.r.guerra@outlook.com>
2020-11-12 01:04:56 -0500

    - Added System Tray functionality - Added logging - Added Docstrings

--- themeswitch/__main__.py
+++ themeswitch/__main__.py
@@ -3,9 +3,89 @@ import themeswitch.functions as functions
 import tkinter as tk
 from pathlib import Path
 import argparse
+from pystray import Menu, MenuItem
+import pystray
+import time
+from PIL import Image
+
+logger = functions.get_logger(__name__)
+logger.info("Starting program.")
+
+
+def setup(icon):
+    """
+    Make the system tray icon visible and notifies the user
+
+    :param icon: Icon object that is displayed on the System Tray
+    :type icon: :class:`pystray.Icon`
+    :return: None
+    :rtype: None
+    """
+    logger.info("Program moved to System Tray.")
+    icon.visible = True
+    icon.notify("Theme switch has moved to the system tray.")
+    time.sleep(3)
+    icon.remove_notification()
+
+
+def reopen_program(root, icon):
+    """
+    Redraw the gui window and hide System Tray icon.
+
+    :param root: Main GUI Window
+    :type root: :class:`tk.Tk`
+    :param icon: Icon object that is displayed on the System Tray
+    :type icon: :class:`pystray.Icon`
+    :return: None
+    :rtype: None
+    """
+    logger.info("Program opened from System Tray.")
+    icon.visible = False
+    icon.stop()
+    root.deiconify()
+
+
+def exit_tray(root, icon):
+    """
+    Close the program completely. Destroy the Main GUI window and stop the program from running on the System Tray
+
+    :param root: Main GUI Window
+    :type root: :class:`tk.Tk`
+    :param icon: Icon object that is displayed on the System Tray
+    :type icon: :class:`pystray.Icon`
+    :return: None
+    :rtype: None
+    """
+    logger.info("Closing program.")
+    icon.visible = False
+    icon.stop()
+    root.destroy()
+
+
+def change_mode_tray(settings):
+    """
+    Change to Dark or Light mode (Whichever is inactive) from the System Tray
+
+    :param settings: A dictionary with the contents of `settings.yaml`
+    :type settings: dict
+    :return: None
+    :rtype: None
+    """
+    mode = 'dark_mode' if functions.light_mode_is_on() else 'light_mode'
+    logger.info("Changed to %s", mode)
+    values = settings[mode]
+    functions.change_sys_theme(values['brightness'], values['wallpaper'], values['os_theme'])
 
 
 def main():
+    """
+    Load settings and check the status of scheduled tasks. Parse and run with the arguments invoked when running the
+    program if any. If no arguments where invoked, open the GUI and move the program to System Tray when closed.
+
+    :return: None
+    :rtype: None
+    """
+
     settings = functions.load_settings()
     functions.check_tasks(settings)
     ap = argparse.ArgumentParser()
@@ -18,13 +98,24 @@ def main():
             values = settings[mode]
         else:
             values = settings['dark_mode'] if functions.light_mode_is_on() else settings['light_mode']
-        functions.change_sys_theme(**values)
+        functions.change_sys_theme(values['brightness'], values['wallpaper'], values['os_theme'])
     else:
+        def on_closing():
+            icon = pystray.Icon("ThemeSwitch")
+            icon.icon = Image.open(Path(__file__).parent / "../icons/icon.ico")
+            icon.title = "Theme Switch"
+            icon.menu = Menu(
+                MenuItem('Open', lambda: reopen_program(root, icon), default=True),
+                MenuItem('Change mode', lambda: change_mode_tray(settings)),
+                MenuItem('Quit', lambda: exit_tray(root, icon))
+            )
+            root.withdraw()
+            icon.run(setup)
         root = tk.Tk()
-        print(f'{Path(__file__).parent / "../icons/icon.ico"=}')
         root.iconbitmap(True, Path(__file__).parent / "../icons/icon.ico")
         root.title("Theme Switcher")
         gui.MainWindow(root)
+        root.protocol("WM_DELETE_WINDOW", on_closing)
         root.mainloop()
 
 
